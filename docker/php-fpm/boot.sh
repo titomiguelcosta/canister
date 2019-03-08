46cb6a33be3034b60c114673410c0cc0ce76775d
Tito Miguel Costa <titomiguelcosta@gmail.com>
2019-03-08 18:16:09 +1300

    run composer install when booting project

--- docker/php-fpm/boot.sh
+++ docker/php-fpm/boot.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+composer install
 php bin/console doctrine:migrations:migrate --no-interaction --all-or-nothing
 php bin/console doctrine:fixtures:load --no-interaction --append
 /usr/bin/php-fpm
+
